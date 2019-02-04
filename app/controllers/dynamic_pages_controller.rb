95f0a82625cd6c5e345f0b236976ea4e5c99e103
Amine42 <amine.bouraghda@gmail.com>
2019-02-04 22:38:43 +0100

    route modified

---
+++ app/controllers/dynamic_pages_controller.rb
@@ -0,0 +1,5 @@
+class DynamicPagesController < ApplicationController
+  def welcome
+    @first_name = params[:first_name]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/team', to: 'static_pages#team'
   get '/contact', to: 'static_pages#contact'
+
+  get 'welcome/:first_name', to: 'dynamic_pages#welcome'
 end
