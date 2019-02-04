e4d99ef8396d628e2718b37cf9cbc9aa963ba0b1
Amine42 <amine.bouraghda@gmail.com>
2019-02-04 15:23:10 +0100

    seeds.rb modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker' # on inclus la gem faker qui avs nous permettre de generer des donnees
+
+# on vide toutes les tables
+
+User.destroy_all
+City.destroy_all
+Gossip.destroy_all
+Tag.destroy_all
+PrivateMessage.destroy_all
+Comment.destroy_all
+Like.destroy_all
+
+# on fait 10 tours de boucle ici
+10.times do
+  # on remplis la table City avec des donnee cree par faker
+  ville = City.create!(name: Faker::Address.city,zip_code: Faker::Address.zip_code)
+end
+
+# on fait 10 tours de boucle ici
+10.times do
+  # on remplis la table User avec des donnee cree par faker
+  utilisateur = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph,email: Faker::Internet.email, age: rand(18..75),city_id: rand((City.first.id)..(City.last.id)))
+end
+
+# on fait 10 tours de boucle ici
+10.times do
+  # on remplis la table Gossip avec des donnee cree par faker
+ tweety = Gossip.create!(title: Faker::Lorem.words(8),content: Faker::Lorem.sentence,user_id: rand((User.first.id)..(User.last.id)))
+end
+
+# on fait 10 tours de boucle ici
+10.times do
+  # on remplis la table Tag avec des donnee cree par faker
+  style = Tag.create!(title: Faker::Lorem.words(8),gossip_id: rand((Gossip.first.id)..(Gossip.last.id)))
+end
+
+# on fait 10 tours de boucle ici
+10.times do
+  # on remplis la table PrivateMessage avec des donnee cree par faker
+  pm = PrivateMessage.create!(content: Faker::Lorem.sentence,sender_id: rand((User.first.id)..(User.last.id)),recipient_id: rand((User.first.id)..(User.last.id)))
+end
