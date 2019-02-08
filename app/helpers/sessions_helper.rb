7b72278b774e2af23d37197dc41b373fa87e84c7
hehomarie <marie.goudon@laposte.net>
2019-02-08 09:40:01 +0100

    ajout de lindex

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,14 @@
 module SessionsHelper
+  before_action :authenticate_user, only: [:index]
    def current_user
     User.find_by(id: session[:user_id])
   end
+
+  def index
+    html+= "<%= "Comment", comment_path %>"
+    html+="<%="Like",like_path %>"
+    html+= "<%= "Create Gossip", create_path %>"
+    html
 
    def log_in(user)
     session[:user_id] = user.id
@@ -15,6 +22,16 @@ module SessionsHelper
       return true
     elsif session[:user_id] == nil
       return false
+    end
+  end
+
+  private
+
+ 
+  def authenticate_user
+    unless current_user
+      flash[:danger] = "please log in."
+      redirect_to new_session_path
     end
   end
 end
