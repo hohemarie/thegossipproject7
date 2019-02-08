735e7fb66f600f12f48c1f338619ef9a07deb4bd
hehomarie <marie.goudon@laposte.net>
2019-02-08 09:14:31 +0100

    before action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   before_action :logged_in?, only: [:create]
+  before_action :logged_in?, only: [:like]
+  before_action :logged_in?, only: [:comment]
+  before_action :logged_in?, only: [:show]
+  before_action :logged_in?, only: [:edit]
   def index
   end
 
