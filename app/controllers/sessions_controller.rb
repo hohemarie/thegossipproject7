d16b5650fbc1f3576b736dd5d1b4196e55f61eb4
hehomarie <marie.goudon@laposte.net>
2019-02-08 09:19:28 +0100

    ajoute lien et methode destroy et lien se deconnecter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   before_action :logged_in?, only: [:edit]
   def index
   end
-
+  def destroy
+    session.delete(:user_id)
+  end
   def create
     potin = Gossip.create(id_potin: @id_potin, content: @content, user_id: @user_id)
   end
