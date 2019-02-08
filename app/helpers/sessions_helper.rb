52db3ccf055e6070bfb3c0388a8f1700fd694fe8
hehomarie <marie.goudon@laposte.net>
2019-02-08 09:28:21 +0100

    logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+   def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+   def log_in(user)
+    session[:user_id] = user.id
+  end
+  def logged_in?
+    if session[:user_id] != nil
+      return true
+    elsif session[:user_id] == nil
+      return false
+    end
+  end
 end
